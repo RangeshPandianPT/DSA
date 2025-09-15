f01880fcb87e50d06f507a225b18aefe6e5a70ed
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-09-15 17:47:46 +0530

    Add files via upload

---
+++ DSA_C/Day-12-Abstract classes.c
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Book {
+protected:
+    string title;
+    string author;
+public:
+    Book(string t, string a) {
+        title = t;
+        author = a;
+    }
+    virtual void display() = 0; 
+};
+
+class MyBook : public Book {
+private:
+    int price;
+public:
+    
+    MyBook(string t, string a, int p) : Book(t, a) {
+        price = p;
+    }
+
+    
+    void display() override {
+        cout << "Title: " << title << "\n";
+        cout << "Author: " << author << "\n";
+        cout << "Price: " << price << "\n";
+    }
+};
+
+int main() {
+    string title, author;
+    int price;
+    
+    getline(cin, title);
+    getline(cin, author);
+    cin >> price;
+    
+    MyBook novel(title, author, price);
+    novel.display();
+
+    return 0;
+}
