8400f258817c31e7b62279afa349206890f0bc53
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-09-29 17:00:54 +0530

    Add files via upload

---
+++ DSA_JAVA/Generics.java
@@ -0,0 +1,16 @@
+public class Generics {
+    
+    public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+
+    public static void main(String args[]) {
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = { "Hello", "World" };
+
+        printArray(intArray);
+        printArray(stringArray);
+    }
+}
