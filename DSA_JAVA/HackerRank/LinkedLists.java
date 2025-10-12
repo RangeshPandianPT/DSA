2172ff1941f6f12d0dc06849b17e32a4d5c38699
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-10-12 08:26:16 +0530

    Add files via upload

---
+++ DSA_JAVA/HackerRank/LinkedLists.java
@@ -0,0 +1,58 @@
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class LinkedLists {
+    public static Node removeDuplicates(Node head) {
+        if (head == null) return null;
+
+        Node current = head;
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+                // skip duplicate node
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    public static Node insert(Node head, int data) {
+        Node p = new Node(data);
+        if (head == null)
+            head = p;
+        else {
+            Node start = head;
+            while (start.next != null)
+                start = start.next;
+            start.next = p;
+        }
+        return head;
+    }
+
+    public static void display(Node head) {
+        Node start = head;
+        while (start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        Node head = null;
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head, ele);
+        }
+        head = removeDuplicates(head);
+        display(head);
+    }
+}
