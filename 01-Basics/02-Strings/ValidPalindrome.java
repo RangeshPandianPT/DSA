1f626186735d6b8ca14f68195e39f533efd955dc
RangeshPandianPT <rangeshpandian@gmail.com>
2026-06-03 08:47:47 +0530

    Update Basics section files

---
+++ 01-Basics/02-Strings/ValidPalindrome.java
@@ -0,0 +1,19 @@
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "racecar";
+        System.out.println("Is '" + s + "' a palindrome? " + isPalindrome(s));
+    }
+}
