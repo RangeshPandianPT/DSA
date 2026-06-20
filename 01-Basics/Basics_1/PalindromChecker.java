621139fe0fc93bba12c42e731e10c512f611570a
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2026-06-20 09:48:58 +0530

    Palindrome Checker in java

---
+++ 01-Basics/Basics_1/PalindromChecker.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Locale;
+
+public class PalindromChecker {
+    public static void main(String[] args) {
+        String str = "Tamil";
+        System.out.println(isPalindromeCheck((str)));
+
+    }
+
+    static boolean isPalindromeCheck(String str) {
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length() / 2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length() - 1 - i);
+
+            if (start != end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
