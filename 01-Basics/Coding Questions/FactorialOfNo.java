a31221d3de1f73241d4761866c7c4561d37a8371
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2026-06-21 12:58:07 +0530

    Finding the Factorial of a number in java

--- 01-Basics/Coding Questions/FactorialOfNo.java
+++ 01-Basics/Coding Questions/FactorialOfNo.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
 public class FactorialOfNo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        long fact = 1;
+
+        for (int i = 1;i<=n;i++){
+            fact *=i;
+        }
+        System.out.println(fact);
+    }
 }
