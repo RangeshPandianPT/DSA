6c3af498b7e5ebe6198e54236f1f0baeb3991e76
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2026-06-22 22:37:33 +0530

    Updated file with findAllIndex2 function call in code

--- 01-Basics/Recursion/Find.java
+++ 01-Basics/Recursion/Find.java
@@ -1,3 +1,5 @@
+package Recursion;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -6,12 +8,13 @@ public class Find {
         int[] arr = {1, 2, 5, 7, 8, 5, 7};
         boolean ans = isPresent(arr, 7, 0);
         int ans2 = findIndex(arr, 7, 0);
-        int ans3 = findIndexLast(arr, 5, arr.length-1);
-        findAllIndex(arr, 7, 1);
+        int ans3 = findIndexLast(arr, 5, arr.length - 1);
+        ArrayList<Integer> ans4 = findAllIndex(arr, 7, 1, new ArrayList<>());
         System.out.println(ans);
         System.out.println(ans2);
         System.out.println(ans3);
-        System.out.println(list);
+        System.out.println(ans4);
+        System.out.println(findAllIndex2(arr,7,0));
     }
 
     static boolean isPresent(int[] arr, int target, int index) {
@@ -42,15 +45,27 @@ public class Find {
         }
     }
 
-        static ArrayList<Integer> list = new ArrayList<Integer>();
-
-        static void findAllIndex ( int[] arr, int target, int index){
-            if (index == arr.length) {
-                return;
-            }
-            if (arr[index] == target) {
-                list.add(index);
-            }
-            findAllIndex(arr, target, index + 1);
+    static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
         }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        return findAllIndex(arr, target, index + 1, list);
     }
+
+    static ArrayList<Integer> findAllIndex2(int[] arr, int target, int index) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (index == arr.length) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        ArrayList<Integer> ansFromBelowCalls = findAllIndex2(arr, target, index + 1);
+        list.addAll(ansFromBelowCalls);
+
+        return list;
+    }
+}
