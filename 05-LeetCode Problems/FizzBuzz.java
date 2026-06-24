1b3c7d961ab4956730946a18cd479af15e6b4419
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2026-06-24 18:11:34 +0530

    412. Fizz Buzz solution in java

---
+++ 05-LeetCode Problems/FizzBuzz.java
@@ -0,0 +1,33 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+    int n = 15;
+    System.out.println(fizzBuzz(n));
+
+    }
+    public static  List<String> fizzBuzz(int n){
+        List<String> answer = new ArrayList();
+        for (int i = 1; i <= n; i++){
+            boolean divisibleBy3 = (i%3 == 0);
+            boolean divisibleBy5 = (i%5 == 0);
+
+            if (divisibleBy3 && divisibleBy5){
+                answer.add("FizzBuzz");
+            }
+            else if (divisibleBy3){
+                answer.add("Fizz");
+            }
+            else if (divisibleBy5){
+                answer.add("Buzz");
+            }
+            else{
+                answer.add(Integer.toString(i));
+            }
+        }
+        return answer;
+    }
+}
