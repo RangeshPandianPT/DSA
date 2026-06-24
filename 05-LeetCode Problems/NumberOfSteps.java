97e7a805ee2a98c9b3ce73edb83222567c373238
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2026-06-24 17:29:52 +0530

    finding number of steps to reach 0 in java

---
+++ 05-LeetCode Problems/NumberOfSteps.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class NumberOfSteps {
+    public static void main(String[] args) {
+        int num = 14;
+        System.out.println(numberOfSteps(num));
+    }
+    static int numberOfSteps (int num){
+        int steps = 0;
+        while ( num > 0){
+            if (num%2 == 0){
+                num/=2;
+            }
+            else {
+                num--;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
