6473f45336595a27719647e4291f39619bd47a32
RangeshPandianPT <rangeshpandian@gmail.com>
2026-06-26 20:44:40 +0530

    Add Reverse Linked List algorithm

---
+++ 02-Data-Structures/04-Linked-Lists/ReverseLinkedList.java
@@ -0,0 +1,45 @@
+public class ReverseLinkedList {
+    static class Node {
+        int data;
+        Node next;
+        public Node(int data) { this.data = data; }
+    }
+
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        
+        while (current != null) {
+            next = current.next; // store next
+            current.next = prev; // reverse pointer
+            prev = current; // advance prev
+            current = next; // advance current
+        }
+        return prev;
+    }
+
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        head = reverse(head);
+
+        System.out.println("Reversed List:");
+        printList(head);
+    }
+}
