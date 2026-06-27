be443ba69d9a4269211e714369c839707b08feb4
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2026-06-27 21:13:36 +0530

    148. Sort List

---
+++ 05-LeetCode Problems/MergeSorting.java
@@ -0,0 +1,66 @@
+package LinkedList;
+
+import LinkedList.ListNode;
+
+class MergeSorting {
+
+    public ListNode sortList(ListNode head) {
+        return mergeSort(head);
+    }
+
+    private ListNode mergeSort(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = getMid(head);
+
+        ListNode left = mergeSort(head);
+        ListNode right = mergeSort(mid);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+
+            tail = tail.next;
+        }
+
+        tail.next = (l1 != null) ? l1 : l2;
+
+        return dummy.next;
+    }
+
+    private ListNode getMid(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (prev != null) {
+            prev.next = null;
+        }
+
+        return slow;
+    }
+}
